e00af1e147fb561f4073df199f180faae6084806
NANDAN-S-GMIT <benz.nandan.s@gmail.com>
2025-04-09 20:20:27 +0530

    working2

---
+++ js/questions.js
@@ -0,0 +1,237 @@
+let questions = [
+  {
+      numb: 1,
+      question: "What does HTML stand for?",
+      answer: "Hyper Text Markup Language",
+      options: [
+          "Hyper Text Preprocessor",
+          "Hyper Text Markup Language",
+          "Hyper Text Multiple Language",
+          "Hyper Tool Multi Language"
+      ]
+  },
+  {
+      numb: 2,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options: [
+          "Common Style Sheet",
+          "Colorful Style Sheet",
+          "Computer Style Sheet",
+          "Cascading Style Sheet"
+      ]
+  },
+  {
+      numb: 3,
+      question: "What does PHP stand for?",
+      answer: "Hypertext Preprocessor",
+      options: [
+          "Hypertext Preprocessor",
+          "Hypertext Programming",
+          "Hypertext Preprogramming",
+          "Hometext Preprocessor"
+      ]
+  },
+  {
+      numb: 4,
+      question: "What does SQL stand for?",
+      answer: "Structured Query Language",
+      options: [
+          "Stylish Question Language",
+          "Stylesheet Query Language",
+          "Statement Question Language",
+          "Structured Query Language"
+      ]
+  },
+  {
+      numb: 5,
+      question: "What does XML stand for?",
+      answer: "eXtensible Markup Language",
+      options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+      ]
+  },
+  {
+      numb: 6,
+      question: "Which programming language is known as the 'mother of all languages'?",
+      answer: "C",
+      options: [
+          "Java",
+          "C",
+          "Python",
+          "Assembly"
+      ]
+  },
+  {
+      numb: 7,
+      question: "What is the main purpose of JavaScript?",
+      answer: "To add interactivity to web pages",
+      options: [
+          "To style web pages",
+          "To add interactivity to web pages",
+          "To define website structure",
+          "To store data"
+      ]
+  },
+  {
+      numb: 8,
+      question: "Which tag is used to define an unordered list in HTML?",
+      answer: "ul",
+      options: [
+          "ol",
+          "li",
+          "ul",
+          "list"
+      ]
+  },
+  {
+      numb: 9,
+      question: "Which CSS property is used to change text color?",
+      answer: "color",
+      options: [
+          "text-color",
+          "font-color",
+          "color",
+          "background-color"
+      ]
+  },
+  {
+      numb: 10,
+      question: "What is the default programming language of web browsers?",
+      answer: "JavaScript",
+      options: [
+          "Python",
+          "Java",
+          "JavaScript",
+          "C++"
+      ]
+  },
+  {
+      numb: 11,
+      question: "Which symbol is used for comments in JavaScript?",
+      answer: "//",
+      options: [
+          "//",
+          "/* */",
+          "#",
+          "!"
+      ]
+  },
+  {
+      numb: 12,
+      question: "What is the output of 2 + '2' in JavaScript?",
+      answer: "22",
+      options: [
+          "4",
+          "22",
+          "Error",
+          "NaN"
+      ]
+  },
+  {
+      numb: 13,
+      question: "Which function is used to print in Python?",
+      answer: "print()",
+      options: [
+          "echo()",
+          "print()",
+          "console.log()",
+          "document.write()"
+      ]
+  },
+  {
+      numb: 14,
+      question: "Which database uses tables, rows, and columns?",
+      answer: "Relational Database",
+      options: [
+          "NoSQL Database",
+          "Relational Database",
+          "Graph Database",
+          "Key-Value Database"
+      ]
+  },
+  {
+      numb: 15,
+      question: "Which keyword is used to declare a variable in JavaScript?",
+      answer: "let",
+      options: [
+          "var",
+          "let",
+          "const",
+          "All of the above"
+      ]
+  },
+  {
+      numb: 16,
+      question: "Which protocol is used to transfer web pages?",
+      answer: "HTTP",
+      options: [
+          "FTP",
+          "HTTP",
+          "SMTP",
+          "SSH"
+      ]
+  },
+  {
+      numb: 17,
+      question: "Which of the following is a backend language?",
+      answer: "Node.js",
+      options: [
+          "HTML",
+          "CSS",
+          "Node.js",
+          "XML"
+      ]
+  },
+  {
+      numb: 18,
+      question: "What is the full form of API?",
+      answer: "Application Programming Interface",
+      options: [
+          "Application Program Integration",
+          "Application Processing Interface",
+          "Application Programming Interface",
+          "Automated Programming Interface"
+      ]
+  },
+  {
+      numb: 19,
+      question: "Which JavaScript function is used to convert a string to an integer?",
+      answer: "parseInt()",
+      options: [
+          "parseInt()",
+          "toInteger()",
+          "stringToInt()",
+          "convert()"
+      ]
+  },
+  {
+      numb: 20,
+      question: "Which of these is NOT a valid JavaScript data type?",
+      answer: "Character",
+      options: [
+          "String",
+          "Number",
+          "Character",
+          "Boolean"
+      ]
+  }
+];
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
